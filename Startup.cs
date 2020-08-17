2fd63516972828501eb000eaa80cd393bd7086c3
ortegajosant <ortega.josant@gmail.com>
2020-08-16 18:10:27 -0600

    Test

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace proyectoprogramado
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Juliana, LA AMOOOOOOOOOOOOOO! :3");
                 });
             });
         }

9d107ea58ce0f0ce9405da0fe3f9f9ee2ae2251c
Jose Ortega <37756558+ortegajosant@users.noreply.github.com>
2020-08-18 01:16:34 -0600

    Fields and data to database flow are added (#5)

---
+++ DBTables/DBCampusContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace proyectoprogramado.DBTables
+{
+    public class DBCampusContext : DbContext
+    {
+
+        public DBCampusContext(DbContextOptions<DBCampusContext> options)
+        : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:campustec-server.database.windows.net,1433;Initial Catalog=campustec-db;Persist Security Info=False;User ID=specials-admin;Password=Spec123.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+        public DbSet<User> User
+        {
+            get;
+            set;
+        }
+    }
+}

8758ac004559d1123143b044ee244e4cd4f5ad96
Jose Ortega <37756558+ortegajosant@users.noreply.github.com>
2020-08-18 16:32:23 -0600

    Added (#9)

---
+++ DBTables/Student.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace proyectoprogramado.DBTables
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public int idUser { get; set; }
+        public string universidad { get; set; }
+        public string sede { get; set; }
+        public string email1 { get; set; }
+        public string email2 { get; set; }
+        public string mobilePhone { get; set; }
+        public int amountOfTec_colones { get; set; }
+    }
+
+}

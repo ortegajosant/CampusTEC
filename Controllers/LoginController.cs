6b1ecf002ee3362162c1f5805790940e8ecc0b0c
tsg3 <estebandcg1999@gmail.com>
2020-08-17 20:40:36 -0600

    Login implemented (#4)
    * Creacion del LoginView (#2)
    
    * Login model controller (#3)
    
    * Login model base added
    
    * Login controller base added
    
    * Model-View-Controller implemented
    
    * Login autocomplete off
    
    Co-authored-by: vicarhz <vic.arhz@gmail.com>

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,34 +4,56 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using proyectoprogramado.Models;
 
 namespace proyectoprogramado.Controllers
 {
-    [ApiController]
-    [Route("campustec/login")]
     public class LoginController : Controller
     {
+
+        private LoginModel model;
+
+        public LoginController (){
+            LoginModel model = new LoginModel();
+            this.set_model(model);
+        }
+
+        public void set_model(LoginModel model){
+            this.model = model;
+        }
+
+        public bool login_access(string id, string password){
+            bool result = model.check_user(id, password);
+            return result; 
+        }
+
+        [HttpPost]
+        public IActionResult Login(string id, string password)
+        {
+            TempData["id"] = id;
+            TempData["password"] = password;
+            bool result = login_access(id, password);
+            if(result == true)
+                return RedirectToAction("Perfil", "Login");
+            else
+                return RedirectToAction("Login", "Login");
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Login()
         {
-            return new string[] { "Initial", "Hello" };
+            return View();
         }
 
-        [HttpPost("{id}/{pass}")]
-        public IEnumerable<string> Post(int id, string pass)
+        [HttpGet]
+        public IActionResult Perfil()
         {
-            if (id == 0){
-                return new string[] { id.ToString() , pass };
-            }
-            else{
-                return new string[] {"You are my friend aahhhh"};
-            }
-            
+            string param1 = TempData["id"].ToString();
+            string param2 = TempData["password"].ToString();
+            TempData["param1"] = param1;
+            TempData["param2"] = param2;
+            return View();
         }
 
-        [HttpGet("{id}")]
-        public string Get(int id){
-            return "value";
-        }
     }
 }
